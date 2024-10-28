413ccdeac138b709ebe26dfd8935fd8e32c052de
webidare <webidevolution@gmail.com>
2024-10-29 03:42:48 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { Header } from "@/components/header";
 import { Hero } from "@/components/blocks/hero";
 import { Cases } from "@/components/blocks/cases";
-import { Testimonials } from "@/components/blocks/testimonials";
 import { Features } from "@/components/blocks/features";
 import { Pricings } from "@/components/blocks/pricings";
 import { Stats } from "@/components/blocks/stats";
