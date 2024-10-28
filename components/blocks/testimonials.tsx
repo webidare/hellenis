c1c9d81fc8650cf2caac04731316a3aff8f2674d
webidare <webidevolution@gmail.com>
2024-10-29 03:42:02 +0700

    Update testimonials.tsx

--- components/blocks/testimonials.tsx
+++ components/blocks/testimonials.tsx
@@ -1,64 +1,78 @@
-import TestimonialSlider from "@/components/testimonials-slider";
+"use client";
 
-const testimonials = [
-  {
-    quote:
-      "Nostrud tempor sunt fugiat. Dolor in sint dolore labore non occaecat adipisicing Lorem labore ullamco enim excepteur. In fugiat Lorem sit velit id veniam esse eiusmod non ea voluptate cupidatat reprehenderit ullamco dolore. Mollit laborum occaecat aliquip.",
-    name: "Rose Roberson",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=1",
-  },
-  {
-    quote:
-      "Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation. Culpa consectetur dolor pariatur commodo aliqua amet tempor nisi enim deserunt elit cillum.",
-    name: "Chace Rodgers",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=10",
-  },
-  {
-    quote:
-      "Id duis velit enim officia ad nisi incididunt magna ex dolor minim deserunt dolor.",
-    name: "Cornelius Sheppard",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=9",
-  },
-  {
-    quote:
-      "Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
-    name: "Chace Rodgers",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=7",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
-    name: "Cornelius Sheppard",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=8",
-  },
-  {
-    quote:
-      "Consectetur voluptate pariatur dolore laboris. Eiusmod dolor aute ut nulla pariatur officia consequat aute amet exercitation.",
-    name: "Chace Rodgers",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=2",
-  },
-  {
-    quote:
-      "Id duis velit enim officia ad nisi incididunt magna ex dolor minim deserunt dolor.",
-    name: "Cornelius Sheppard",
-    role: "CEO at Company",
-    imgSrc: "https://i.pravatar.cc/120?img=3",
-  },
-];
+import Image from "next/image";
+import { imageLoader } from "@/lib/imageLoader";
 
-export default function Home() {
+import Autoplay from "embla-carousel-autoplay";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Testimonial } from "@/types/testimonial";
+
+interface TestimonialSliderProps {
+  testimonials: Testimonial[];
+}
+
+export default function TestimonialSlider({
+  testimonials,
+}: TestimonialSliderProps) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-5">
-      <div className="container">
-        <h1>Autoplay plugin</h1>
-        <TestimonialSlider testimonials={testimonials} />
-      </div>
-    </main>
+    <>
+      <section className="w-full py-4">
+        <div className="mx-auto lg:max-w-6xl px-3">
+          <Carousel
+            opts={{
+              loop: true,
+              align: "start",
+            }}
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
+          >
+            <CarouselContent>
+              {testimonials.map((testimonial, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                  <div className="flex flex-col px-4 py-5 sm:p-6">
+                    <q className="flex-1 text-gray-600 dark:text-gray-300">
+                      {testimonial.quote}
+                    </q>
+                    <div className="mt-6 flex gap-3">
+                      <span className="inline-flex rounded-full">
+                        <Image
+                          loader={imageLoader}
+                          className="h-10 w-10 rounded-full"
+                          height={40}
+                          width={40}
+                          alt={testimonial.name}
+                          src={testimonial.imgSrc}
+                          loading="lazy"
+                        />
+                      </span>
+                      <div>
+                        <p className="text-sm font-semibold text-gray-900 dark:text-white">
+                          {testimonial.name}
+                        </p>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">
+                          {testimonial.role}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="absolute left-[-50px] top-1/2 -translate-y-1/2 fill-black" />
+            <CarouselNext className="absolute right-[-50px] top-1/2 -translate-y-1/2 fill-black" />
+          </Carousel>
+        </div>
+      </section>
+    </>
   );
 }
