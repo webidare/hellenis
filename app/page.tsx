9eb19be54e10ae19f738ce87f37dda7a919e2214
webidare <181756637+webidare@users.noreply.github.com>
2024-10-28 19:05:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Header } from "@/components/header";
+import { Hero } from "@/components/blocks/hero";
+import { Cases } from "@/components/blocks/cases";
+import { Testimonials } from "@/components/blocks/testimonials";
+import { Features } from "@/components/blocks/features";
+import { Pricings } from "@/components/blocks/pricings";
+import { Stats } from "@/components/blocks/stats";
+import { CTAs } from "@/components/blocks/ctas";
+import { Blogs } from "@/components/blocks/blogs";
+import { FAQs } from "@/components/blocks/faqs";
+import { Contacts } from "@/components/blocks/contacts";
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Cases />
+      <Testimonials />
+      <Features />
+      <Pricings />
+      <Stats />
+      <CTAs />
+      <Blogs />
+      <FAQs />
+      <Contacts />
+    </>
+  );
+}
