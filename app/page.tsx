7d55664c2ad7765efae372ef385a11d1b16d21a8
webidare <webidevolution@gmail.com>
2024-10-29 13:22:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { Header } from "@/components/header";
 import { Hero } from "@/components/blocks/hero";
-import { Cases } from "@/components/blocks/cases";
+import Cases from "@/components/blocks/cases";
 import { Features } from "@/components/blocks/features";
 import { Pricings } from "@/components/blocks/pricings";
 import { Stats } from "@/components/blocks/stats";
